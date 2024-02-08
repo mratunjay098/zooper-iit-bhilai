b2fbc77220244505791e7659922b0e278367bf7f
Mratunjay Singh <mratunjays@iitbhilai.ac.in>
2024-02-08 15:47:58 +0530

    updatedWebsite

--- websiteForInference/static/script.js
+++ websiteForInference/static/script.js
@@ -1,6 +1,11 @@
 function predictSpeed() {
+    const selectedModel = document.getElementById('model-select').value;
+    const checkpoint = document.getElementById('checkpoint').value;
+    const timestamp = document.getElementById('timestamp').value;
+    const traffic = document.getElementById('traffic').value;
+    const intersection = document.getElementById('intersection').value;
+    const roadHierarchy = document.getElementById('road-hierarchy').value;
     const latLongInput = document.getElementById('latLong').value;
-    const selectedModel = document.getElementById('modelSelect').value;
     
     // Extract latitude and longitude from the input
     const [latitude, longitude] = latLongInput.split(',').map(coord => coord.trim());
@@ -10,14 +15,27 @@ function predictSpeed() {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ latitude, longitude, selectedModel })
+        body: JSON.stringify({ selectedModel, checkpoint, timestamp, traffic, intersection, roadHierarchy, latitude, longitude })
     })
     .then(response => response.json())
     .then(data => {
+        const predictionText = document.getElementById('prediction-text');
+
         if (data.success) {
-            document.getElementById('prediction').innerText = `Predicted Speed: ${data.prediction.toFixed(2)}`;
+            const predictedSpeed = parseInt(data.prediction);
+            predictionText.innerHTML = `Predicted Speed: ${predictedSpeed} <br>`;
+
+            // Check if the predicted speed is less than or equal to 60
+            if (predictedSpeed <= 60) {
+                predictionText.style.color = 'green';
+                predictionText.innerHTML += ' <span style="font-weight: bold;">(Good Driver)</span>';
+            } else {
+                predictionText.style.color = 'red';
+                predictionText.innerHTML += ' <span style="font-weigxht: bold;">(Bad Driver)</span>';
+            }
         } else {
-            document.getElementById('prediction').innerText = `Error: ${data.error}`;
+            predictionText.innerText = `Error: ${data.error}`;
+            predictionText.style.color = 'red';
         }
     });
 
